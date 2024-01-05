4ef31640929ab6ce54bc52bdc94cd50114a4657e
CometArao <abel.rosales2001@alumnos.ubiobio.cl>
2024-01-05 17:38:51 -0300

    Primera subida de apuntes

---
+++ JSProfesional-CodigoFacilito/Primersaludo.js
@@ -0,0 +1,26 @@
+// Console log nos permite imprimir en la consola/terminal
+console.log("Hola mundo");
+
+/* En el caso de alert se imprime en una ventana emergente, pero no es recomendable usarlo
+ ya que detiene la ejecución del código. */
+// alert("Hola mundo");
+
+/* Prompt nos ayuda a pedir datos al usuario a través de una ventana emergente. Luego
+dará como resultado lo que el usuario haya ingresado */
+// prompt("¿Cuál es tu nombre?");
+
+/* Javascript es un lenguaje débilmente y dinámicamente tipado.
+ Puede asignar tipos de variables sin necesidad de declararlos manualmente, y estos 
+pueden cambiar a lo largo del código.
+
+De la misma forma, JS solo reconocerá los tipos de variables cuando se ejecute el código,
+no antes. */
+
+let edad = 25;
+
+edad = "25 años";
+
+/* También distingue entre mayúsculas y minúsculas. */
+
+let nombre = "Juan";
+let Nombre = "Pedro";
